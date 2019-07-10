66d0c263b1ce5621e549aeb6b16afc0b4532205e
Coursera Learner <nobody@nowhere.nul>
2019-07-10 14:46:10 +0000

    DId assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,36 +1,60 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct _retire_info{
+#include <stdio.h>
+#include <stdlib.h>
+
+struct _retire_info {
   int months;
   double contribution;
   double rate_of_return;
 };
+
 typedef struct _retire_info retire_info;
 
-void retirement (int startAge,double initial , retire_info working, retire_info retired)
-{
-  
-  float balance=initial;
-  for(int i=0;i<working.months;i++)
-    {
-      printf("Age %3d month %2d you have $%.2lf\n",startAge/12,startAge%12,balance);
-  balance =balance + balance*(working.rate_of_return)+working.contribution;
-  
-  
-  startAge++;
-    }
-  for(int i=0;i<retired.months;i++)
-    {
-      
-  
-      printf("Age %3d month %2d you have $%.2lf\n",startAge/12,startAge%12,balance);
-      balance =balance + balance*(retired.rate_of_return)+retired.contribution;
-      startAge++;
-    }
+double next_balance(double balance, retire_info ri) {
+
+  return balance * ( 1 + ri.rate_of_return ) + ri.contribution;
+
 }
-int main (void)
-{
-  
+
+void calc_year_month(int months, int *year, int *month) {
+
+  *year = months / 12;
+  *month = months % 12;
+
+}
+
+void print_balance_info(int months, double balance) {
+
+  int age, month;
+
+  calc_year_month(months, &age, &month);
+  printf("Age %3d month %2d you have $%.2lf\n", age, month, balance);
+
+}
+
+void retirement(int startAge, // in months
+		double initial, // initial savings in dollar
+		retire_info working,
+		retire_info retired) {
+
+  int m;
+  int age_months = startAge;
+  double balance = initial;
+
+  for (m=0; m<working.months; m++) {
+    print_balance_info(age_months, balance);
+    age_months++;
+    balance = next_balance(balance, working);
+  }
+
+  for (m=0; m<retired.months; m++) {
+    print_balance_info(age_months, balance);
+    age_months++;
+    balance = next_balance(balance, retired);
+  }
+
+}
+
+int main(void) {
 
   retire_info working = {
     489, 1000.0, 0.045 / 12
@@ -39,13 +63,15 @@ int main (void)
   retire_info retired = {
     384, -4000.0, 0.01 / 12
   };
+
   int start_age_months = 327;
   double savings = 21345;
 
-  retirement (start_age_months,
+  retirement(start_age_months,
 	     savings,
 	     working,
 	     retired);
 
   return EXIT_SUCCESS;
+
 }
